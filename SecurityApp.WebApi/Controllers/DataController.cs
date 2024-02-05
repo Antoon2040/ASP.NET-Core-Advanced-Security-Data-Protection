cb580808328fa22229407aff2a48b2963bf95f1f
Antoon2040 <antoon2030@gmail.com>
2024-02-05 19:10:44 +0200

    video 4

---
+++ SecurityApp.WebApi/Controllers/DataController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Mvc;
+using SecurityApp.WebApi.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SecurityApp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class DataController : Controller
+    {
+        private readonly IDataProtectionProvider _dataProtectionProvider;
+        public DataController(IDataProtectionProvider dataProtectionProvider)
+        {
+            _dataProtectionProvider = dataProtectionProvider;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            string title = "Welcome to this course!";
+
+            var dataprotector = _dataProtectionProvider.CreateProtector("DataController");
+            var protectedTitle = dataprotector.Protect(title);
+            var unProtectedTitle = dataprotector.Unprotect(protectedTitle);
+
+            return Ok(new DataProtection()
+            {
+                Title = title,
+                ProtectedTitle = protectedTitle,
+                UnProtectedTitle = unProtectedTitle 
+            });
+        }
+    }
+}
+
---
+++ SecurityApp.WebApi/Data/DataProtection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace SecurityApp.WebApi.Data
+{
+	public class DataProtection
+	{
+		public string Title { get; set; }
+
+		public string ProtectedTitle { get; set; }
+
+        public string UnProtectedTitle { get; set; }
+
+    }
+}
+
