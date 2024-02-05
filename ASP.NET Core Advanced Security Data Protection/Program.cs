ce21ab4481633ae3166fd6e2a2bfde922fb50b88
Antoon2040 <antoon2030@gmail.com>
2024-02-05 18:44:24 +0200

    vidoe 3

--- ASP.NET Core Advanced Security Data Protection/Program.cs
+++ ASP.NET Core Advanced Security Data Protection/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace ASP.NET_Core_Advanced_Security_Data_Protection;
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ASP.NET_Core_Advanced_Security_Data_Protection;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // 1. Data protection services
+        var servicesCollection = new ServiceCollection();
+        servicesCollection.AddDataProtection();
+        var services = servicesCollection.BuildServiceProvider();
+        // 2. Create an instance of Idataprotection interface
+        var dataProtectionProvider = services.GetService<IDataProtectionProvider>();
+        var dataProtector = dataProtectionProvider.CreateProtector("FirstExample");
+        // 3. protect and unprotect a payload
+        string title = "Welcome to this course!";
+        Console.WriteLine($"Original value ={title}");
+
+        var protectedTitle = dataProtector.Protect(title);
+        Console.WriteLine($"Protected value ={protectedTitle}");
+
+        var unProtectedTitle = dataProtector.Unprotect(protectedTitle);
+        Console.WriteLine($"Unprotected value ={unProtectedTitle}");
+
+        Console.ReadLine();
     }
 }
 
